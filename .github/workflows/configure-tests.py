0b29b081f48d69e67c186bb76d04b78e5af6f3e4
Michael Still <mikal@stillhq.com>
2024-04-25 18:47:21 +1000

    Add the rest of the tests.

--- .github/workflows/configure-tests.py
+++ .github/workflows/configure-tests.py
@@ -5,6 +5,38 @@ import jinja2
 JOBS = {
     'functional-tests': [
         {
+            'name': 'rocky-9-kolla',
+            'baseimage': 'sf://label/ci-images/rocky-9',
+            'baseuser': 'cloud-user',
+            'targets': [
+                'kolla-2023.1', 'kolla-2023.2', 'kolla'
+                ]
+        },
+        {
+            'name': 'debian-12-kolla',
+            'baseimage': 'sf://label/ci-images/debian-12',
+            'baseuser': 'debian',
+            'targets': [
+                'kolla-2023.1', 'kolla-2023.2', 'kolla'
+                ]
+        },
+        {
+            'name': 'rocky-9-kolla-ansible',
+            'baseimage': 'sf://label/ci-images/rocky-9',
+            'baseuser': 'cloud-user',
+            'targets': [
+                'kolla-ansible-2023.1', 'kolla-ansible-2023.2', 'kolla-ansible'
+                ]
+        },
+        {
+            'name': 'debian-12-kolla-ansible',
+            'baseimage': 'sf://label/ci-images/debian-12',
+            'baseuser': 'debian',
+            'targets': [
+                'kolla-ansible-2023.1', 'kolla-ansible-2023.2', 'kolla-ansible'
+                ]
+        },
+        {
             'name': 'rocky-9-nova',
             'baseimage': 'sf://label/ci-images/rocky-9',
             'baseuser': 'cloud-user',
@@ -15,6 +47,78 @@ JOBS = {
             'baseimage': 'sf://label/ci-images/debian-12',
             'baseuser': 'debian',
             'targets': ['nova-2023.1', 'nova-2023.2', 'nova-2024.1', 'nova']
+        },
+        {
+            'name': 'rocky-9-openstacksdk',
+            'baseimage': 'sf://label/ci-images/rocky-9',
+            'baseuser': 'cloud-user',
+            'targets': [
+                'openstacksdk-2023.1', 'openstacksdk-2023.2',
+                'openstacksdk-2024.1', 'openstacksdk'
+                ]
+        },
+        {
+            'name': 'debian-12-openstacksdk',
+            'baseimage': 'sf://label/ci-images/debian-12',
+            'baseuser': 'debian',
+            'targets': [
+                'openstacksdk-2023.1', 'openstacksdk-2023.2',
+                'openstacksdk-2024.1', 'openstacksdk'
+                ]
+        },
+        {
+            'name': 'rocky-9-oslo.config',
+            'baseimage': 'sf://label/ci-images/rocky-9',
+            'baseuser': 'cloud-user',
+            'targets': [
+                'oslo.config-2023.1', 'oslo.config-2023.2',
+                'oslo.config-2024.1', 'oslo.config'
+                ]
+        },
+        {
+            'name': 'debian-12-oslo.config',
+            'baseimage': 'sf://label/ci-images/debian-12',
+            'baseuser': 'debian',
+            'targets': [
+                'oslo.config-2023.1', 'oslo.config-2023.2',
+                'oslo.config-2024.1', 'oslo.config'
+                ]
+        },
+        {
+            'name': 'rocky-9-python-novaclient',
+            'baseimage': 'sf://label/ci-images/rocky-9',
+            'baseuser': 'cloud-user',
+            'targets': [
+                'python-novaclient-2023.1', 'python-novaclient-2023.2',
+                'python-novaclient-2024.1', 'python-novaclient'
+                ]
+        },
+        {
+            'name': 'debian-12-python-novaclient',
+            'baseimage': 'sf://label/ci-images/debian-12',
+            'baseuser': 'debian',
+            'targets': [
+                'python-novaclient-2023.1', 'python-novaclient-2023.2',
+                'python-novaclient-2024.1', 'python-novaclient'
+                ]
+        },
+        {
+            'name': 'rocky-9-python-openstackclient',
+            'baseimage': 'sf://label/ci-images/rocky-9',
+            'baseuser': 'cloud-user',
+            'targets': [
+                'python-openstackclient-2023.1', 'python-openstackclient-2023.2',
+                'python-openstackclient-2024.1', 'python-openstackclient'
+                ]
+        },
+        {
+            'name': 'debian-12-python-openstackclient',
+            'baseimage': 'sf://label/ci-images/debian-12',
+            'baseuser': 'debian',
+            'targets': [
+                'python-openstackclient-2023.1', 'python-openstackclient-2023.2',
+                'python-openstackclient-2024.1', 'python-openstackclient'
+                ]
         },
     ],
 }
