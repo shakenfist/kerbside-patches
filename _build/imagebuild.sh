f737503c53e5193a6b5dd8af98f57215bdbdce86
Michael Still <mikal@stillhq.com>
2025-04-08 18:30:33 +1000

    Refactor for 2025.1 builds.

--- _build/imagebuild.sh
+++ _build/imagebuild.sh
@@ -113,7 +113,7 @@ for target in ${build_targets}; do
     # Customize the kolla-build.conf file
     echo
     echo -e "${H2}Customize build configuration${Color_Off}"
-    cat kolla-build.conf.in | \
+    cat etc/kolla-build-${target}.conf.in | \
         sed "s|TOPSRCDIR|${topsrcdir}|g" \
         > kolla-build.conf
 
