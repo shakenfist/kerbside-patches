1df467c8052b05bd8332436f5a1efe66ba971bd8
Michael Still <mikal@stillhq.com>
2025-10-01 14:10:05 +1000

    Include patched source in checksum.

--- _build/calculate-container-hash.sh
+++ _build/calculate-container-hash.sh
@@ -8,21 +8,35 @@ unique=$(date "+%Y%m%d")
 topdir=$(pwd)
 
 for dir in ${*}; do
-    cd ${dir}
-    if [ -e PREPATCH ]; then
-        for patch in $(cat PREPATCH); do
-            hash=$(sha1sum ${patch} | cut -f 1 -d " " | sed -rn 's/^(........).*/\1/gp')
+    if [ ! -e ${dir} ]; then
+        echo "Hash directory ${dir} does not exist." >&2
+    else
+        cd ${dir}
+        
+        if [ "${dir}" == "src" ]; then
+            # `src` is handled differently
+            hash=$(tar c . | sha1sum - | cut -f 1 -d " " | sed -rn 's/^(........).*/\1/gp')
             unique="${unique};${hash}"
-        done
-    fi
+        else
+            # And the others are assumed to be directories of patches
+            if [ -e PREPATCH ]; then
+                for patch in $(cat PREPATCH); do
+                    hash=$(sha1sum ${patch} | cut -f 1 -d " " | sed -rn 's/^(........).*/\1/gp')
+                    unique="${unique};${hash}"
+                done
+            fi
 
-    if [ -e ORDER ]; then
-        for patch in $(cat ORDER); do
-            hash=$(sha1sum ${patch} | cut -f 1 -d " " | sed -rn 's/^(........).*/\1/gp')
-            unique="${unique};${hash}"
-        done
+            if [ -e ORDER ]; then
+                for patch in $(cat ORDER); do
+                    hash=$(sha1sum ${patch} | cut -f 1 -d " " | sed -rn 's/^(........).*/\1/gp')
+                    unique="${unique};${hash}"
+                done
+            fi
+        fi
+
+        echo "Hash directory ${dir} is ${hash}." >&2
+        cd ${topdir}
     fi
-    cd ${topdir}
 done
 
 echo "v12-${unique}" | sha1sum | cut -f 1 -d " " | sed -rn 's/^(........).*/\1/gp'
