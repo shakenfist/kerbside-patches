57d07b77eb8a0d9895ebffd3fbc169fdea7c980d
Michael Still <mikal@stillhq.com>
2025-11-23 06:37:16 +1100

    Its easier if add-insecure-ci-registry doesn't depend on common.

--- _build/add-insecure-ci-registry.sh
+++ _build/add-insecure-ci-registry.sh
@@ -1,7 +1,32 @@
 #!/bin/bash -e
 
-# Run from the top directory.
-. _build/common.sh
+# Some code from common.sh is copied here so that this script can be stand
+# alone.
+
+export Color_Off='\033[0m'       # Text Reset
+export Red='\033[0;31m'          # Red
+export Green='\033[0;32m'        # Green
+
+export H1="${Green}"
+
+# Make failures more obvious
+function on_exit {
+    echo
+    echo -e "${Red}*** Failed ***${No_Color}"
+    echo
+    exit 1
+    }
+trap 'on_exit $?' EXIT
+
+function banner {
+    echo
+    echo -e "${H1}**************************************************${Color_Off}"
+    echo -e "${H1}${1}${Color_Off}"
+    echo -e "${H1}**************************************************${Color_Off}"
+    echo
+}
+
+
 
 banner "Setup HTTP CI registry"
 
