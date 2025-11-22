9ff25724a75798f9c53f44762dadbfde76cee103
Michael Still <mikal@stillhq.com>
2025-11-22 12:16:42 +1100

    Configure multinode registries.

--- _build/add-insecure-ci-registry.sh
+++ _build/add-insecure-ci-registry.sh
@@ -6,6 +6,7 @@
 banner "Setup HTTP CI registry"
 
 sudo mkdir -p /etc/docker/
+sudo rm -f /etc/docker/daemon.json
 cat - <<EOF | sudo tee /etc/docker/daemon.json
 {
     "insecure-registries" : [ "192.168.1.12:4000" ]
