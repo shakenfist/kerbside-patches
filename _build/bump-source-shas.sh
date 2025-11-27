371abd2c263e9020cc7fdc3dfd4171c36b56f1b2
Michael Still <mikal@stillhq.com>
2025-11-28 06:42:50 +1100

    Backports don't always use merges.

--- _build/bump-source-shas.sh
+++ _build/bump-source-shas.sh
@@ -9,17 +9,19 @@ for project in ${projects}; do
     mkdir src
     repo=$(yq -r .repo ${project}/config.yaml)
     source_branch=$(yq -r .source_branch ${project}/config.yaml)
+    previous_source_sha=$(yq -r .source_sha ${project}/config.yaml)
     directory=$(yq -r .directory ${project}/config.yaml)
 
     git clone --branch ${source_branch} ${repo} src/${directory}
     cd src/${directory}
-    source_sha=$(git log --merges | grep -E "^commit" | head -${1} | tail -1 | cut -f 2 -d " ")
+    source_sha=$(git log --pretty=format:%H | head -${1} | tail -1 | cut -f 2 -d " ")
     cd ${cwd}
     rm -rf src
 
     echo
     echo "Setting ${project} source_sha to ${source_sha}"
     yq -i -y ".source_sha = \"${source_sha}\"" ${project}/config.yaml
+    yq -i -y ".previous_source_sha = \"${previous_source_sha}\"" ${project}/config.yaml
 
     echo
     echo
