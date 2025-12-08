c43aa7f0e4dbb35b452fe3550f8efabb92a027d7
Michael Still <mikal@stillhq.com>
2025-12-08 19:06:45 +1100

    Fix bugs in pinning to new SHAs.

--- _build/bump-source-shas.sh
+++ _build/bump-source-shas.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # $1 is how many merges to go back in time.
+if [ -z ${1} ]; then
+    rewind=1
+else
+    rewind=${1}
+fi
+
+echo "We will go back ${rewind} commits, with 1 being HEAD."
 
 # Lock in new daily SHAs for each project we're patching
 cwd=$(pwd)
@@ -14,14 +21,18 @@ for project in ${projects}; do
 
     git clone --branch ${source_branch} ${repo} src/${directory}
     cd src/${directory}
-    source_sha=$(git log --pretty=format:%H | head -${1} | tail -1 | cut -f 2 -d " ")
+    source_sha=$(git log --pretty=format:%H | head -${rewind} | tail -1 | cut -f 2 -d " ")
     cd ${cwd}
     rm -rf src
 
     echo
-    echo "Setting ${project} source_sha to ${source_sha}"
-    yq -i -y ".source_sha = \"${source_sha}\"" ${project}/config.yaml
-    yq -i -y ".previous_source_sha = \"${previous_source_sha}\"" ${project}/config.yaml
+    if [ "${source_sha}" != "${previous_source_sha}" ]; then
+        echo "Setting ${project} source_sha to ${source_sha}"
+        yq -i -y ".source_sha = \"${source_sha}\"" ${project}/config.yaml
+        yq -i -y ".previous_source_sha = \"${previous_source_sha}\"" ${project}/config.yaml
+    else
+	echo "SHA unchanged."
+    fi
 
     echo
     echo
