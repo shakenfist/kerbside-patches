4f863b8c239446f72a2bd596c1648d6505b45d79
Michael Still <mikal@stillhq.com>
2026-02-18 20:54:05 +1100

    Try out dockerpush with occystrap.

--- _build/build-containers.sh
+++ _build/build-containers.sh
@@ -136,7 +136,7 @@ for target in ${build_targets}; do
                     --password ${registry_token} \
                     --layer-cache "${topdir}/archive/occystrap-layer-cache.json" \
                     process \
-                    "docker://${image}:${complete_image_tag}" \
+                    "dockerpush://${image}:${complete_image_tag}" \
                     "registry://${ci_registry}/${registry_project}/${registry_image}:${complete_image_tag}?insecure=true" \
                     -f "inspect:file=${layers_dir}/${safe_name}-as-built.jsonl" \
                     -f normalize-timestamps \
